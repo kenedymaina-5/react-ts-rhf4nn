c7a6efd5486e69ee37c6d0a5af13fc3fef367143
kenedymaina-5 <76390492+kenedymaina-5@users.noreply.github.com>
2023-05-21 14:12:11 +0300

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,30 +1,31 @@
-import * as React from 'react';
+import React from 'react';
 import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Reactjs</h1>
-      <p>The following article is dedicate to react enthunsiasts</p>
-      <p>
-        {' '}
-        When it comes to running React.js code online, there are several
-        platforms available that allow you to create and test your applications.
-        One such platform is StackBlitz, which provides a seamless online
-        development environment. In the provided code snippet, a basic React.js
-        application is defined using functional components. The App component is
-        exported as the default component for the application. Inside the App
-        component, a div element is created to contain the content of the
-        application. Within the div, there is an h1 element with the text "Hello
-        StackBlitz!" and a p element with the text "Start editing to see some
-        magic happen :)". These elements are styled using the CSS file imported
-        through the import './style.css' statement. By utilizing StackBlitz or
-        similar online platforms, you can easily run and test your React.js
-        code. Simply make changes to the code, save it, and observe the magic
-        happen in real-time as the application updates and renders the changes
-        you made. These platforms provide a convenient way to experiment,
-        collaborate, and showcase your React.js projects.
-      </p>
+      <nav>
+        <ul className="navbar">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">About</li>
+          <li className="nav-item">Services</li>
+          <li className="nav-item">Contact</li>
+        </ul>
+      </nav>
+      <div className="container">
+        <div className="sidebar">
+          <ul className="sidebar-menu">
+            <li className="menu-item">Blog</li>
+            <li className="menu-item">Author</li>
+            <li className="menu-item">Books</li>
+          </ul>
+        </div>
+        <div className="content">
+          <h1>Reactjs</h1>
+          <p>The following article is dedicated to React enthusiasts.</p>
+          <p>When it comes to running React.js code online, there are several platforms available that allow you to create and test your React.js projects.</p>
+        </div>
+      </div>
     </div>
   );
 }
